6fe688defb4f0c7c878e1ae74f91878c2f45720d
eachmobi <eachmobi@gmail.com>
2025-04-22 09:14:21 +0800

    update img and css

--- static/main.js
+++ static/main.js
@@ -1,8 +1,8 @@
 // preloader
 $(window).on('load', function () {
-      $("#status").fadeOut();
-      $("#preloader").delay(100).fadeOut("fast");
-      $("body").delay(100).css({overflow: "visible"});
+    $("#status").fadeOut();
+    $("#preloader").delay(100).fadeOut("fast");
+    $("body").delay(100).css({overflow: "visible"});
 });
 
 $(function() {
@@ -33,4 +33,25 @@ $(function() {
         $('.navbar-collapse').collapse('hide');
     });
 });
+
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.wheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("wheel", handle, { passive: true });
+    }
+};
+jQuery.event.special.mousewheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("mousewheel", handle, { passive: true });
+    }
+};
 
